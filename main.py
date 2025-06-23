3089a9124899fcdba0cfac741d9a10aae32e11df
hanshygod <kayasamsung19@gmail.com>
2025-06-23 23:55:00 +0200

    Spam main

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time, requests, pyfiglet, threading
 print(pyfiglet.figlet_format("KINGMAN"))
 
-msg = input("Please Insert WebHook Spam Message: ")
-webhook = input("Please Insert WebHook URL: ")
-th = int(input('Number of thread ? (200 recommended): '))
-sleep = int(input("Sleeping time ? (recommended 2): "))
+msg = input("Get fucking pwned by memtar and balls out niggas easy webhook i made incase my mod got removed")
+webhook = input("https://discord.com/api/webhooks/1377803501836566561/tWg813LN7J_wWpgTwlKD2KCbRxKOFd98urxfLjx0aWaxQKwRsAtJoJH_M7hQ5AAXioiQ")
+th = int(input('200'))
+sleep = int(input("2"))
 def spam():
     while True:
         try:
