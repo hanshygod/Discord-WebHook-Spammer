af0e0835d0eafe0171f005462a3b31b33f706fc6
hanshygod <kayasamsung19@gmail.com>
2025-06-23 23:51:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time, requests, pyfiglet, threading
 print(pyfiglet.figlet_format("KINGMAN"))
 
-msg = input("Please Insert WebHook Spam Message: ")
-webhook = input("Please Insert WebHook URL: ")
+msg = input("get pwned by memtar and balls out niggas fuck this server")
+webhook = input("https://discord.com/api/webhooks/1377803501836566561/tWg813LN7J_wWpgTwlKD2KCbRxKOFd98urxfLjx0aWaxQKwRsAtJoJH_M7hQ5AAXioiQ")
 th = int(input('Number of thread ? (200 recommended): '))
 sleep = int(input("Sleeping time ? (recommended 2): "))
 def spam():
